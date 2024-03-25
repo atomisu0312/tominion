46e55cefdfe2d8303beefe2bae7f1830a5f261c3
takahiro-maekawa <koma4024@gmail.com>
2024-03-26 00:35:02 +0900

    ヘッダに表示するアイコンを作ろう #10
    - ついでにImageWithFallBackについてコメントを追記

--- front/tominion-next/src/app/ui/util/image/imageWithFallBack.tsx
+++ front/tominion-next/src/app/ui/util/image/imageWithFallBack.tsx
@@ -1,8 +1,10 @@
 'use client';
+// https://solutions-image-fallback.vercel.app/を参考にしました。
 
 import React, { useState, useEffect } from 'react';
 import Image, { ImageProps } from 'next/image';
 
+// 元々のPropsで必須となっているものは、継承によってそのまま使える
 interface Props extends ImageProps{
   src: string,
   fallback: string,
@@ -15,14 +17,15 @@ const ImageWithFallback = ({
 }:Props) => {
   const [error, setError] = useState(false)
 
+  // srcが変わったら一度errorをfalseにして様子を見る
   useEffect(() => {
     setError(false)
   }, [src])
 
   return (
     <Image
-      onError={() =>setError(true)}
-      src={error ? fallback : src}
+      onError={() => setError(true)} // Errorが検知されたら即、errorをtrueに
+      src={error ? fallback : src} // ここはまあわかる
       {...props}
     />
   )
