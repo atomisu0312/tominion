2e9296e3539783420870a19d3096799acb67c4ce
takahiro-maekawa <koma4024@gmail.com>
2024-04-10 22:29:08 +0900

    SelectButtonコンポーネントを作成(card>search>kind) #30 - CSSを調整する - ボタンとsliceを紐づける - storeが更新されることを確認する

--- front/tominion-next/src/app/ui/card/search/cardKind.tsx
+++ front/tominion-next/src/app/ui/card/search/cardKind.tsx
@@ -1,44 +1,45 @@
 import KindSelectButton from "./kind/kindSelectButton";
 import KindLabel from "./kind/kindLabel";
-import AttrLabel from "./kind/attrLabel";
-import AttrSelectButton from "./kind/attrSelectButton";
+import AttrLabel from "./attr/attrLabel";
+import AttrSelectButton from "./attr/attrSelectButton";
+import {CardKind} from "@/lib/features/home/card/search/cardKind";
 
-export default function CardKind(){
+export default function CardKindComponent(){
   return(
   <>
-    <div className=" bg-green-400 h-full">
-    <div className="grid grid-flow-col grid-rows-6 h-full">
+    <div className="h-full">
+      <div className="grid grid-flow-col grid-rows-6 h-full">
         <div className ="col-span-1 row-span-2">
           <KindLabel labelStr = "種別"/>
         </div>
         <div className ="col-span-1 row-span-1">
-          <KindSelectButton labelStr = "アクション"/>
-        </div>
-        <div className ="row-span-1">
-          <KindSelectButton labelStr = "財宝"/>
-        </div>
-        <div className ="row-span-1">
-          <KindSelectButton labelStr = "勝利点"/>
-        </div>
-        <div className ="row-span-1">
-          <KindSelectButton labelStr = "呪い"/>
-        </div>
-        <div className ="col-span-1 row-span-2">
-          <KindLabel labelStr = "属性"/>
+          <KindSelectButton labelStr = "アクション" cardKind={CardKind.action} />
         </div>
         <div className ="col-span-1 row-span-1">
-          <KindSelectButton labelStr = "なし"/>
+          <KindSelectButton labelStr = "財宝" cardKind={CardKind.treasure}/>
         </div>
-        <div className ="row-span-1">
-          <KindSelectButton labelStr = "アタック"/>
+        <div className ="col-span-1 row-span-1">
+          <KindSelectButton labelStr = "勝利点" cardKind={CardKind.victory}/>
         </div>
-        <div className ="row-span-1">
-          <KindSelectButton labelStr = "リアクション"/>
+        <div className ="col-span-1 row-span-1">
+          <KindSelectButton labelStr = "呪い" cardKind={CardKind.curse} />
         </div>
-        <div className ="row-span-1">
-          <KindSelectButton labelStr = "持続"/>
+        <div className ="col-span-2 row-span-2">
+          <AttrLabel labelStr = "属性"/>
         </div>
-    </div>
+        <div className ="col-span-2 row-span-1">
+          <AttrSelectButton labelStr = "なし"/>
+        </div>
+        <div className ="col-span-2 row-span-1">
+          <AttrSelectButton labelStr = "アタック"/>
+        </div>
+        <div className ="col-span-2 row-span-1">
+          <AttrSelectButton labelStr = "リアクション"/>
+        </div>
+        <div className ="col-span-2 row-span-1">
+          <AttrSelectButton labelStr = "持続"/>
+        </div>
+      </div>
     </div>
   </>)
 } 
--- front/tominion-next/src/app/ui/card/search/searchResult.tsx
+++ front/tominion-next/src/app/ui/card/search/searchResult.tsx
@@ -5,6 +5,7 @@ export default function SearchResult(){
   const costMin = useSelector((state: RootState) => state.cardSearch.costMin);
   const costMax = useSelector((state: RootState) => state.cardSearch.costMax);
   const searchWord = useSelector((state: RootState) => state.cardSearch.searchWord);
+  const cardKindList = useSelector((state: RootState) => state.cardSearch.cardKindList);
 
   return(
   <>
@@ -15,6 +16,9 @@ export default function SearchResult(){
     </div>
     <div className="w-full">
       <span>{`検索ワード：${searchWord}`}</span>
+    </div>
+    <div className = "w-full">
+      <span>{`種別：${cardKindList}`}</span>
     </div>
   </>)
 } 
