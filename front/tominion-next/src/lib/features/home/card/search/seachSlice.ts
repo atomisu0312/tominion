3aae5359abf5569bed13d6634435514c5013353f
takahiro-maekawa <koma4024@gmail.com>
2024-04-10 22:24:34 +0900

    SelectButtonコンポーネントを作成(card>search>kind)#30 - 共通で使うenumを準備 - sliceを定義

--- front/tominion-next/src/lib/features/home/card/search/seachSlice.ts
+++ front/tominion-next/src/lib/features/home/card/search/seachSlice.ts
@@ -1,16 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import type { CardKind } from "@/lib/features/home/card/search/cardKind";
 
 type State ={
   costMin: undefined | number;
   costMax: undefined | number;
   searchWord: undefined | String;
+  cardKindList: CardKind[];
 }
 
 const initialState: State = {
   costMin: undefined,
   costMax: undefined,
-  searchWord: undefined
+  searchWord: undefined,
+  cardKindList: [],
 }
 
 export const searchSlice =  createSlice({
@@ -38,8 +41,17 @@ export const searchSlice =  createSlice({
     },
     setSearchWord:(state, action:PayloadAction<String>) => {
       state.searchWord = action.payload;
+    },
+    // cardKindListに追加したり除外したりする関数を追加する
+    handleCardKindList:(state, action:PayloadAction<CardKind>) => {
+      const index = state.cardKindList.indexOf(action.payload);
+      if (index === -1){
+        state.cardKindList.push(action.payload);
+      }else{
+        state.cardKindList.splice(index, 1); 
+      }
     }
   }
 });
-export const {setCostMin, setCostMax, setCostMinMax, setSearchWord} = searchSlice.actions;
+export const {setCostMin, setCostMax, setCostMinMax, setSearchWord, handleCardKindList} = searchSlice.actions;
 export default searchSlice.reducer;
